f7cf32ee89c7fa6e448c06cb71bfa8d32f0b7122
SleepyBear-UI <daffamantap29@gmail.com>
2025-03-23 21:40:33 +0700

    menambah class sayatubevideo

--- TPMOD6_103022300056/Program.cs
+++ TPMOD6_103022300056/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using TPMOD6_103022300056;
 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Daffa Khairi Putra F");
+        video.IncreasePlayCount(10);
+        video.PrintVideoDetail();
+    }
+}
---
+++ TPMOD6_103022300056/SayaTubeVideo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace TPMOD6_103022300056
+{
+	public class SayaTubeVideo
+	{
+		private int id;
+		private String title;
+		private int playCount;
+
+		private static Random random = new Random();
+
+        public SayaTubeVideo(String title)
+        {
+
+            this.id = random.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int addPlay)
+		{
+			this.playCount += addPlay;
+		}
+
+		public void PrintVideoDetail()
+		{
+			Console.WriteLine($"ID : {this.id}, Title : {this.title}, playCount : {this.playCount}");
+		}
+	}
+}
